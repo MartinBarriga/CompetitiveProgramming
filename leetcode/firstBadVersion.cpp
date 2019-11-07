8429e6c129309682179fccc81559669de6ce0984
Martín Eduardo Barriga Vargas <martin.barriga.vargas@gmail.com>
2019-11-07 16:44:39 -0600

    AC

---
+++ leetcode/firstBadVersion.cpp
@@ -0,0 +1,24 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int mid;
+        while(left <= right){
+            mid = left + (right-left)/2;
+            if(isBadVersion(mid) && (mid == 1 || !isBadVersion(mid-1))){
+                break;
+            }
+            if(isBadVersion(mid)){
+                right = mid-1;
+            }
+            else{
+                left = mid +1;
+            }
+        }
+        return mid;
+    }
+};
