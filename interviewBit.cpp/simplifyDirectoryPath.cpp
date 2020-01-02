a2655bdc63635c2d41580884cb7a361f43b0ea71
Martín Eduardo Barriga Vargas <martin.barriga.vargas@gmail.com>
2020-01-01 21:29:34 -0600

    AC

---
+++ interviewBit.cpp/simplifyDirectoryPath.cpp
@@ -0,0 +1,39 @@
+void insertNewDirectory(deque<string> &absPath, int &index, string &A){
+    string folderName = "";
+    while(A[index] != '/'){
+        folderName += A[index];
+        index++;
+    }
+    absPath.push_back(folderName);
+}
+
+string Solution::simplifyPath(string A) {
+    bool lastCharWasASlash = true;
+    deque<string> absPath;
+    string absPathWithSlash = "";
+    for(int index = 0; index < A.size(); index++){
+        if(lastCharWasASlash){
+            if(A[index] == '.'){
+                lastCharWasASlash = false;
+            }
+            else if(A[index] != '/'){
+                insertNewDirectory(absPath, index, A);
+            }
+        }
+        else{
+            if(A[index] == '.'){
+                absPath.pop_back();
+            }
+            
+            lastCharWasASlash = true;
+        }
+    }
+    if(absPath.empty()){
+        return "/";
+    }
+    while(!absPath.empty()){
+        absPathWithSlash += "/" + absPath.front();
+        absPath.pop_front();
+    }
+    return absPathWithSlash;
+}
