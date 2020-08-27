d4dfa2b49e431df47590255aed5382d64838962f
unknown <martin.barriga.vargas@gmail.com>
2020-08-27 02:33:32 -0500

    AC

--- leetcode/rotateArray.cpp
+++ leetcode/rotateArray.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    void invert(vector<int>& nums, int begin, int end){
-        int aux;
-        for(int i = begin; i < (begin + end + 1)/2; i++){
-            aux = nums[i];
-            nums[i] = nums[end+begin-i];
-            nums[end+begin-i] = aux; 
+    void swapArray(int begin, int end, vector<int>& nums){
+        while(begin < end) {
+            swap(nums[begin], nums[end]);
+            begin++;
+            end--;
         }
     }
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        if(k > 0){
-            invert(nums, 0, nums.size()-1);
-            invert(nums, 0, k-1);
-            invert(nums, k, nums.size()-1);
-        }
+        k = k%nums.size();
+        swapArray(0, nums.size()-1, nums);
+        swapArray(0, k - 1, nums);
+        swapArray(k, nums.size() - 1, nums);
     }
 };
