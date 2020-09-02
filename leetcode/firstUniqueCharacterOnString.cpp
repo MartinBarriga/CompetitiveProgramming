bef21a87ecf33c0ad5c7e5ebcd7dd3abb70b26e7
unknown <martin.barriga.vargas@gmail.com>
2020-09-02 18:15:29 -0500

    AC

--- leetcode/firstUniqueCharacterOnString.cpp
+++ leetcode/firstUniqueCharacterOnString.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char, int> letters;
-        for(int i = 0; i < s.size(); i++){
-            if(letters.find(s[i]) == letters.end()){
-                letters[s[i]] = 0;
-            }
-            else{
-                letters[s[i]]++;
+        vector<int> letters(27, 0);
+        int currentLetter;
+        for(int i = 0; i < s.size(); i++) {
+            currentLetter = s[i];
+            if(letters[currentLetter - 'a'] < 2) {
+                letters[currentLetter - 'a']++;   
             }
         }
-        for(int i = 0; i < s.size(); i++){
-            if(letters[s[i]] == 0) return i;
+        
+        for(int i = 0; i < s.size(); i++) {
+            currentLetter = s[i];
+            if(letters[currentLetter - 'a'] == 1) {
+                return i;
+            }
         }
         return -1;
     }
